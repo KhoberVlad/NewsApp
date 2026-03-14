0069da6e62268eef06685edf601679d308fe8df8
Fechut <khober.vflad@gmail.com>
2026-03-14 17:52:58 +0200

    update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Header } from './header/header';
-import { Footer } from './footer/footer';
-import { Card } from './card/card';
-import { NewList } from './new-list/new-list';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Card } from './components/card/card';
+import { Contact } from './components/contact/contact';
+import { AboutUs } from './components/about-us/about-us';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, HttpClientModule, Header, Footer, Card, NewList],
+  imports: [RouterOutlet, CommonModule, HttpClientModule, Header, Footer, Card, Contact, AboutUs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
