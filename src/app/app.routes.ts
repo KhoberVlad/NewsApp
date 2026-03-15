149708694bf0f6e64e68f0de7c0e8913fa383c9d
Fechut <khober.vflad@gmail.com>
2026-03-15 17:52:00 +0200

    add policy-privacy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { AboutUs } from './components/about-us/about-us';
 import { Contact } from './components/contact/contact';
 import { Main } from './components/main/main';
-
+import { PolicyPrivacy } from './components/policy-privacy/policy-privacy';
 export const routes: Routes = [
   { path: '',  redirectTo: 'main', pathMatch: 'full' },
   { path: 'main', component: Main },
   { path: 'about-us', component: AboutUs },
-  { path: 'contact', component: Contact }
+  { path: 'contact', component: Contact },
+  { path: 'privacy', component: PolicyPrivacy }
 ];
